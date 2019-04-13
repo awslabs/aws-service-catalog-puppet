095c38b3d333c08f314e64cc6289746b10fc4b91
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-04-13 23:30:00 +0100

    preparing for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws-service-catalog-puppet",
-    version="0.0.11",
+    version="0.0.13",
     author="Eamonn Faherty",
     author_email="aws-service-catalog-tools@amazon.com",
     description="Making it easier to deploy ServiceCatalog products",
