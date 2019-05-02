e7d62c09ce5317a126711254c29419fd4ff3c095
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-05-02 16:27:13 +0100

    fixing list launches so it works on the puppet account:

--- servicecatalog_puppet/cli.py
+++ servicecatalog_puppet/cli.py
@@ -1,6 +1,5 @@
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-import json
 import shutil
 
 import click
@@ -11,14 +10,13 @@ import os
 from pykwalify.core import Core
 
 from betterboto import client as betterboto_client
-from terminaltables import AsciiTable
-from colorclass import Color
 
+from servicecatalog_puppet.commands.list_launches import do_list_launches
 from servicecatalog_puppet.utils import manifest as manifest_utils
 from servicecatalog_puppet.asset_helpers import resolve_from_site_packages
-from servicecatalog_puppet.constants import LAUNCHES, HOME_REGION, \
+from servicecatalog_puppet.constants import HOME_REGION, \
     CONFIG_PARAM_NAME, CONFIG_PARAM_NAME_ORG_IAM_ROLE_ARN
-from servicecatalog_puppet.core import get_regions, get_org_iam_role_arn, write_templates, create_share_template, \
+from servicecatalog_puppet.core import get_org_iam_role_arn, write_templates, create_share_template, \
     deploy_launches
 from servicecatalog_puppet.commands.bootstrap import do_bootstrap
 from servicecatalog_puppet.commands.bootstrap_spoke import do_bootstrap_spoke
@@ -125,105 +123,8 @@ def seed(complexity, p):
 @cli.command()
 @click.argument('f', type=click.File())
 def list_launches(f):
-    click.echo("Getting details from your account...")
-    ALL_REGIONS = get_regions()
     manifest = manifest_utils.load(f)
-    deployment_map = build_deployment_map(manifest)
-
-    account_ids = [a.get('account_id') for a in manifest.get('accounts')]
-    deployments = {}
-    for account_id in account_ids:
-        for region_name in ALL_REGIONS:
-            role = "arn:aws:iam::{}:role/{}".format(account_id, 'servicecatalog-puppet/PuppetRole')
-            logger.info("Looking at region: {} in account: {}".format(region_name, account_id))
-            with betterboto_client.CrossAccountClientContextManager(
-                    'servicecatalog', role, 'sc-{}-{}'.format(account_id, region_name), region_name=region_name
-            ) as spoke_service_catalog:
-                response = spoke_service_catalog.list_accepted_portfolio_shares()
-                for portfolio in response.get('PortfolioDetails', []):
-                    portfolio_id = portfolio.get('Id')
-                    response = spoke_service_catalog.search_products_as_admin(PortfolioId=portfolio_id)
-                    for product_view_detail in response.get('ProductViewDetails', []):
-                        product_view_summary = product_view_detail.get('ProductViewSummary')
-                        product_id = product_view_summary.get('ProductId')
-                        response = spoke_service_catalog.search_provisioned_products(
-                            Filters={'SearchQuery': ["productId:{}".format(product_id)]})
-                        for provisioned_product in response.get('ProvisionedProducts', []):
-                            launch_name = provisioned_product.get('Name')
-                            status = provisioned_product.get('Status')
-
-                            provisioning_artifact_response = spoke_service_catalog.describe_provisioning_artifact(
-                                ProvisioningArtifactId=provisioned_product.get('ProvisioningArtifactId'),
-                                ProductId=provisioned_product.get('ProductId'),
-                            ).get('ProvisioningArtifactDetail')
-
-                            if deployments.get(account_id) is None:
-                                deployments[account_id] = {'account_id': account_id, 'launches': {}}
-
-                            if deployments[account_id]['launches'].get(launch_name) is None:
-                                deployments[account_id]['launches'][launch_name] = {}
-
-                            deployments[account_id]['launches'][launch_name][region_name] = {
-                                'launch_name': launch_name,
-                                'portfolio': portfolio.get('DisplayName'),
-                                'product': manifest.get('launches').get(launch_name).get('product'),
-                                'version': provisioning_artifact_response.get('Name'),
-                                'active': provisioning_artifact_response.get('Active'),
-                                'region': region_name,
-                                'status': status,
-                            }
-                            output_path = os.path.sep.join([
-                                LAUNCHES,
-                                account_id,
-                                region_name,
-                            ])
-                            if not os.path.exists(output_path):
-                                os.makedirs(output_path)
-
-                            output = os.path.sep.join([output_path, "{}.json".format(provisioned_product.get('Id'))])
-                            with open(output, 'w') as f:
-                                f.write(json.dumps(
-                                    provisioned_product,
-                                    indent=4, default=str
-                                ))
-
-    table = [
-        ['account_id', 'region', 'launch', 'portfolio', 'product', 'expected_version', 'actual_version', 'active',
-         'status']
-    ]
-    for account_id, details in deployment_map.items():
-        for launch_name, launch in details.get('launches').items():
-            if deployments.get(account_id).get('launches').get(launch_name) is None:
-                pass
-            else:
-                for region, regional_details in deployments[account_id]['launches'][launch_name].items():
-                    if regional_details.get('status') == "AVAILABLE":
-                        status = Color("{green}" + regional_details.get('status') + "{/green}")
-                    else:
-                        status = Color("{red}" + regional_details.get('status') + "{/red}")
-                    expected_version = launch.get('version')
-                    actual_version = regional_details.get('version')
-                    if expected_version == actual_version:
-                        actual_version = Color("{green}" + actual_version + "{/green}")
-                    else:
-                        actual_version = Color("{red}" + actual_version + "{/red}")
-                    active = regional_details.get('active')
-                    if active:
-                        active = Color("{green}" + str(active) + "{/green}")
-                    else:
-                        active = Color("{orange}" + str(active) + "{/orange}")
-                    table.append([
-                        account_id,
-                        region,
-                        launch_name,
-                        regional_details.get('portfolio'),
-                        regional_details.get('product'),
-                        expected_version,
-                        actual_version,
-                        active,
-                        status,
-                    ])
-    click.echo(AsciiTable(table).table)
+    do_list_launches(manifest)
 
 
 @cli.command()
