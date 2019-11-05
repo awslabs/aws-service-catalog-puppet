368a9b8c8ca6ac716a6742384e8f5164d14f39a1
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-11-05 16:07:38 +0000

    moving to pipenv

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-with open("servicecatalog_puppet/requirements.txt", "r") as fh:
-    requirements = fh.read().split("\n")
-
-setuptools.setup(
+setup(
     name="aws-service-catalog-puppet",
     version="0.44.2",
     author="Eamonn Faherty",
@@ -18,8 +14,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/awslabs/aws-service-catalog-puppet-framework",
-    packages=setuptools.find_packages(),
-    package_data={'servicecatalog_puppet': ['*','*/*','*/*/*']},
+    packages=find_packages(),
+    package_data={"servicecatalog_puppet": ["*", "*/*", "*/*/*"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -29,8 +25,7 @@ setuptools.setup(
         "Natural Language :: English",
     ],
     entry_points={
-        'console_scripts': [
-            'servicecatalog-puppet = servicecatalog_puppet.cli:cli'
-        ]},
-    install_requires=requirements,
+        "console_scripts": ["servicecatalog-puppet = servicecatalog_puppet.cli:cli"]
+    },
+    install_requires=[],
 )
