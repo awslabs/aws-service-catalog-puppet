99da4723cbb4aec96694f097686c073f7d669cc6
Eamonn Faherty <eamonnf@amazon.co.uk>
2021-06-15 10:14:05 +0100

    Adding more debug

--- servicecatalog_puppet/aws.py
+++ servicecatalog_puppet/aws.py
@@ -230,7 +230,7 @@ def provision_product_with_plan(
                 )
                 logger.info(
                     f"{uid} :: "
-                    f"waiting for provision to complete: {response.get('ProvisionedProductDetail').get('Status')}"
+                    f"waiting for provision to complete: {response.get('ProvisionedProductDetail').get('Status')}: {response.get('ProvisionedProductDetail').get('StatusMessage')}"
                 )
                 provisioned_product_detail = response.get("ProvisionedProductDetail")
                 execute_status = provisioned_product_detail.get("Status")
@@ -392,7 +392,7 @@ def update_provisioned_product(
             )
             logger.info(
                 f"{uid} :: "
-                f"waiting for provision to complete: {response.get('ProvisionedProductDetail').get('Status')}"
+                f"waiting for provision to complete: {response.get('ProvisionedProductDetail').get('Status')}: {response.get('ProvisionedProductDetail').get('StatusMessage')}"
             )
             provisioned_product_detail = response.get("ProvisionedProductDetail")
             execute_status = provisioned_product_detail.get("Status")
