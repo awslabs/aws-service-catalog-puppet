dedf64e26f6e0f2f91f3dcfbb38d3db831e0b498
Eamonn Faherty <eamonnf@amazon.co.uk>
2021-06-23 16:42:16 +0100

    Adding more sleep

--- servicecatalog_puppet/workflow/portfoliomanagement.py
+++ servicecatalog_puppet/workflow/portfoliomanagement.py
@@ -143,7 +143,7 @@ class GetVersionIdByVersionName(PortfolioManagementTask):
         with self.spoke_regional_client("servicecatalog") as servicecatalog:
             version_id = None
             response = servicecatalog.list_provisioning_artifacts_single_page(
-                ProductId=product_id, wait_between_pages=3,
+                ProductId=product_id, wait_between_pages=10,
             )
             for provisioning_artifact_detail in response.get(
                 "ProvisioningArtifactDetails"
