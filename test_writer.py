351f09455f34a75cd22aed6160426dacd7858c59
Eamonn Faherty <eamonnf@amazon.co.uk>
2021-06-30 21:19:56 +0100

    adding test coverage

--- test_writer.py
+++ test_writer.py
@@ -264,7 +264,7 @@ def get_initial_args_for(c):
     return dict()
 
 
-package = "associations"
+package = "associations "
 
 for input in glob.glob("servicecatalog_puppet/workflow/**/*.py", recursive=True):
     print(input)
