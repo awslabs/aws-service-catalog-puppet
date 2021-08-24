90dde7de4de057e336a114e6618967decc21f356
Eamonn Faherty <eamonnf@amazon.co.uk>
2021-08-24 19:38:58 +0100

    adding launch_name to stacks to help migration of launches to stacks

--- servicecatalog_puppet/workflow/stack/terminate_stack_task.py
+++ servicecatalog_puppet/workflow/stack/terminate_stack_task.py
@@ -7,6 +7,7 @@ from servicecatalog_puppet import constants
 from servicecatalog_puppet.workflow import dependency
 from servicecatalog_puppet.workflow.general import get_ssm_param_task
 from servicecatalog_puppet.workflow.stack import provisioning_task
+import functools
 
 
 class TerminateStackTask(
@@ -24,6 +25,7 @@ class TerminateStackTask(
     key = luigi.Parameter()
     version_id = luigi.Parameter()
 
+    launch_name = luigi.Parameter()
     capabilities = luigi.ListParameter()
 
     use_service_role = luigi.BoolParameter()
@@ -56,11 +58,31 @@ class TerminateStackTask(
         requirements = {"section_dependencies": self.get_section_dependencies()}
         return requirements
 
+    @property
+    @functools.lru_cache(maxsize=32)
+    def stack_name_to_use(self):
+        if self.launch_name == "":
+            return self.stack_name
+        else:
+            with self.spoke_regional_client("servicecatalog") as servicecatalog:
+                try:
+                    pp_id = (
+                        servicecatalog.describe_provisioned_product(Name=self.launch_name)
+                        .get("ProvisionedProductDetail")
+                        .get("Id")
+                    )
+                except servicecatalog.exceptions.ResourceNotFoundException as e:
+                    if "Provisioned product not found" in e.response["Error"]["Message"]:
+                        return self.stack_name
+                    else:
+                        raise e
+                return f"SC-{self.account_id}-{pp_id}"
+
     def run(self):
         if self.execution == constants.EXECUTION_MODE_HUB:
             self.terminate_ssm_outputs()
 
         with self.spoke_regional_client("cloudformation") as cloudformation:
-            cloudformation.ensure_deleted(StackName=self.stack_name)
+            cloudformation.ensure_deleted(StackName=self.stack_name_to_use)
 
         self.write_output(self.params_for_results_display())
