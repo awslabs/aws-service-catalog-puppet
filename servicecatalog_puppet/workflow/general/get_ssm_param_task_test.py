13b023d827d26274d40adfbb00d9fd805531c79c
Eamonn Faherty <eamonnf@amazon.co.uk>
2021-09-21 23:16:58 +0100

    fix: improving performance of GenerateManifestWithIdsTask.run

--- servicecatalog_puppet/workflow/general/get_ssm_param_task_test.py
+++ servicecatalog_puppet/workflow/general/get_ssm_param_task_test.py
@@ -57,7 +57,11 @@ class GetSSMParamTaskTest(tasks_unit_tests_helper.PuppetTaskUnitTest):
         # setup
         # exercise
         self.module.GetSSMParamTask(
-            parameter_name=self.parameter_name, name=self.name, region=self.region, path=self.path, recursive=self.recursive,
+            parameter_name=self.parameter_name,
+            name=self.name,
+            region=self.region,
+            path=self.path,
+            recursive=self.recursive,
         )
 
     @skip
