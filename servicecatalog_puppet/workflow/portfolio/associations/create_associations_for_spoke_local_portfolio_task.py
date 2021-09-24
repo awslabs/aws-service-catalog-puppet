346df4c14e440f0c7db75de92199e5229fc8b485
Eamonn Faherty <eamonnf@amazon.co.uk>
2021-09-24 17:13:39 +0100

    feat: can now use wildcards in associations for spoke-local-portfolios

--- servicecatalog_puppet/workflow/portfolio/associations/create_associations_for_spoke_local_portfolio_task.py
+++ servicecatalog_puppet/workflow/portfolio/associations/create_associations_for_spoke_local_portfolio_task.py
@@ -1,5 +1,6 @@
 #  Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
+import re
 
 import luigi
 
@@ -52,10 +53,16 @@ class CreateAssociationsForSpokeLocalPortfolioTask(
         }
 
     def api_calls_used(self):
-        return [
+        calls = [
             f"cloudformation.create_or_update_{self.account_id}_{self.region}",
             f"cloudformation.describe_stacks_{self.account_id}_{self.region}",
         ]
+        used_wildcard = "*" in "".join(self.associations)
+        if used_wildcard:
+            calls.append(
+                f"iam.list_roles_{self.account_id}_{self.region}"
+            )
+        return calls
 
     def run(self):
         create_spoke_local_portfolio_task_input = self.load_from_input(
@@ -71,11 +78,33 @@ class CreateAssociationsForSpokeLocalPortfolioTask(
             nonce="forever",
         )
 
+        used_wildcard = "*" in "".join(self.associations)
+
+        if used_wildcard:
+            roles = list()
+            with self.spoke_regional_client("iam") as iam:
+                paginator = iam.get_paginator('list_roles')
+                for page in paginator.paginate():
+                    roles += page.get("Roles", [])
+
+            associations_to_use = list()
+            for association in self.associations:
+                if "*" in association:
+                    association_as_a_regex = re.escape(association.replace("${AWS::AccountId}", self.account_id)).replace("\\*", "(.*)")
+                    for role_object in roles:
+                        role = role_object.get("Arn")
+                        if re.match(association_as_a_regex, role):
+                            associations_to_use.append(role)
+                else:
+                    associations_to_use.append(association)
+        else:
+            associations_to_use = self.associations
+
         with self.spoke_regional_client("cloudformation") as cloudformation:
             template = config.env.get_template("associations.template.yaml.j2").render(
                 portfolio={
                     "DisplayName": self.portfolio,
-                    "Associations": self.associations,
+                    "Associations": associations_to_use,
                 },
                 portfolio_id=portfolio_id,
             )
