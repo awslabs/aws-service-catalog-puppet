670bf1988e19a5e42008f88dd01aeb6e06f3a88c
Eamonn Faherty <eamonnf@amazon.co.uk>
2022-06-22 10:05:03 +0100

    preparing for release

--- servicecatalog_puppet/commands/misc.py
+++ servicecatalog_puppet/commands/misc.py
@@ -289,48 +289,46 @@ def generate_tasks(
     f, puppet_account_id, executor_account_id, execution_mode, is_dry_run
 ):
     tasks = [
-        # launch_section_task.LaunchSectionTask(
-        #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-        # ),
-        # stack_section_task.StackSectionTask(
-        #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-        # ),
-        # app_section_task.AppSectionTask(
-        #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-        # ),
-        # workspace_section_task.WorkspaceSectionTask(
-        #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-        # ),
-        # assertions_section_task.AssertionsSectionTask(
-        #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-        # ),
+        launch_section_task.LaunchSectionTask(
+            manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+        ),
+        stack_section_task.StackSectionTask(
+            manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+        ),
+        app_section_task.AppSectionTask(
+            manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+        ),
+        workspace_section_task.WorkspaceSectionTask(
+            manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+        ),
+        assertions_section_task.AssertionsSectionTask(
+            manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+        ),
     ]
     if not is_dry_run:
         tasks += [
-            # lambda_invocation_section_task.LambdaInvocationsSectionTask(
-            #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-            # ),
-            # code_build_run_section_task.CodeBuildRunsSectionTask(
-            #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-            # ),
-            # simulate_policy_section_task.SimulatePolicysSectionTask(
-            #     manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-            # ),
+            lambda_invocation_section_task.LambdaInvocationsSectionTask(
+                manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+            ),
+            code_build_run_section_task.CodeBuildRunsSectionTask(
+                manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+            ),
+            simulate_policy_section_task.SimulatePolicysSectionTask(
+                manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+            ),
             spoke_local_portfolio_section_task.SpokeLocalPortfolioSectionTask(
                 manifest_file_path=f.name, puppet_account_id=puppet_account_id,
             ),
         ]
-        # if execution_mode != constants.EXECUTION_MODE_SPOKE:
-        #     tasks.append(
-        #         service_control_policies_section_task.ServiceControlPoliciesSectionTask(
-        #             manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-        #         )
-        #     )
-        #     tasks.append(
-        #         tag_policies_section_task.TagPoliciesSectionTask(
-        #             manifest_file_path=f.name, puppet_account_id=puppet_account_id,
-        #         )
-        #     )
+        if execution_mode != constants.EXECUTION_MODE_SPOKE:
+            tasks += [
+                service_control_policies_section_task.ServiceControlPoliciesSectionTask(
+                    manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+                ),
+                tag_policies_section_task.TagPoliciesSectionTask(
+                    manifest_file_path=f.name, puppet_account_id=puppet_account_id,
+                )
+            ]
 
     return tasks
 
