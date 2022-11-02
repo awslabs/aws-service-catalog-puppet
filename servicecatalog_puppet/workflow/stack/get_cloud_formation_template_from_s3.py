adf35f701d1b2a9acd3d70c7ff09eb8c60b78a3d
Eamonn Faherty <eamonnf@amazon.co.uk>
2022-11-02 21:50:01 +0000

    issue #587 (#588)

--- servicecatalog_puppet/workflow/stack/get_cloud_formation_template_from_s3.py
+++ servicecatalog_puppet/workflow/stack/get_cloud_formation_template_from_s3.py
@@ -2,6 +2,7 @@
 #  SPDX-License-Identifier: Apache-2.0
 import os
 
+import botocore
 import luigi
 
 from servicecatalog_puppet.workflow.dependencies import tasks
@@ -27,19 +28,22 @@ class GetCloudFormationTemplateFromS3(tasks.TaskWithReference):
 
     def run(self):
         with self.hub_client("s3") as s3:
-            regional_template = self.key.replace("-${AWS::Region}", f"-{self.region}")
+            regional_template = self.key.replace("-${AWS::Region}", f"-{self.region}-ffff")
             global_template = self.key.replace("-${AWS::Region}", "")
             p = dict(Bucket=self.bucket)
             if self.version_id != "":
                 p["VersionId"] = self.version_id
 
-            self.debug(f"Trying regional template: {regional_template}")
+            self.info(f"Trying regional template: {regional_template}")
             output = self.output_location
             if not os.path.exists(os.path.dirname(output)):
                 os.makedirs(os.path.dirname(output))
 
             try:
                 s3.download_file(Key=regional_template, Filename=output, **p)
-            except s3.exceptions.NoSuchKey:
-                self.debug(f"Didnt find regional template: {regional_template}")
-                s3.download_file(Key=global_template, Filename=output, **p)
+            except botocore.exceptions.ClientError as e:
+                if "404" == str(e.response['Error']['Code']):
+                    self.info(f"Didnt find regional template: {regional_template}, will try global: {global_template}")
+                    s3.download_file(Key=global_template, Filename=output, **p)
+                else:
+                    raise e
