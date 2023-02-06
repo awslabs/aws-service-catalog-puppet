69c63bc0d42b604c9fbf82d99169e9cec16925b0
Eamonn Faherty <eamonnf@amazon.co.uk>
2023-02-06 10:54:47 +0000

    Fix/optimising run deploy in spoke (#630)
    * fix: moving logic from run-deploy-in-spoke (run per spoke) to generate-manifest-with-ids (run once per execution)
    * fix: making black

--- servicecatalog_puppet/workflow/launch/run_deploy_in_spoke_task.py
+++ servicecatalog_puppet/workflow/launch/run_deploy_in_spoke_task.py
@@ -25,116 +25,17 @@ class RunDeployInSpokeTask(tasks.TaskWithReference):
         generated_manifest = self.get_output_from_reference_dependency(
             self.generate_manifest_ref
         )
-        reference_signed_url = generated_manifest.get("reference_signed_url")
-        manifest_signed_url = generated_manifest.get("manifest_signed_url")
-        cached_output_signed_url = generated_manifest.get("cached_output_signed_url")
-
-        version = constants.VERSION
-        home_region = constants.HOME_REGION
-
-        vars = [
-            {
-                "name": environmental_variables.CACHE_INVALIDATOR,
-                "value": self.cache_invalidator,
-                "type": "PLAINTEXT",
-            },
-            {"name": "VERSION", "value": version, "type": "PLAINTEXT"},
-            {"name": "MANIFEST_URL", "value": manifest_signed_url, "type": "PLAINTEXT"},
-            {
-                "name": "TASK_REFERENCE_URL",
-                "value": reference_signed_url,
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": "PUPPET_ACCOUNT_ID",
-                "value": self.puppet_account_id,
-                "type": "PLAINTEXT",
-            },
-            {"name": "HOME_REGION", "value": home_region, "type": "PLAINTEXT",},
-            {
-                "name": "REGIONS",
-                "value": ",".join(config.get_regions()),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": "SHOULD_COLLECT_CLOUDFORMATION_EVENTS",
-                "value": str(config.get_should_use_sns()),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": "SHOULD_FORWARD_EVENTS_TO_EVENTBRIDGE",
-                "value": str(config.get_should_use_eventbridge()),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": "SHOULD_FORWARD_FAILURES_TO_OPSCENTER",
-                "value": str(config.get_should_forward_failures_to_opscenter()),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": "OUTPUT_CACHE_STARTING_POINT",
-                "value": cached_output_signed_url,
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.IS_CACHING_ENABLED,
-                "value": "False",  # no caching in spokes
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.INITIALISER_STACK_TAGS,
-                "value": config.get_initialiser_stack_tags(),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.GLOBAL_SHARING_MODE,
-                "value": config.get_global_sharing_mode_default(),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.SHOULD_DELETE_ROLLBACK_COMPLETE_STACKS,
-                "value": config.get_should_delete_rollback_complete_stacks(),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.SHOULD_USE_PRODUCT_PLANS,
-                "value": config.get_should_use_product_plans(),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.SPOKE_EXECUTION_MODE_DEPLOY_ENV,
-                "value": config.get_spoke_execution_mode_deploy_env(),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.SCHEDULER_THREADS_OR_PROCESSES,
-                "value": config.get_scheduler_threads_or_processes(),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.GLOBAL_SHARE_TAG_OPTIONS,
-                "value": config.get_global_share_tag_options_default(),
-                "type": "PLAINTEXT",
-            },
-            {
-                "name": environmental_variables.GLOBAL_SHARE_PRINCIPALS,
-                "value": config.get_global_share_principals_default(),
-                "type": "PLAINTEXT",
-            },
-        ]
-
-        if "http" in version:
-            install_command = f"pip install {version}"
-        else:
-            install_command = f"pip install aws-service-catalog-puppet=={version}"
-
-        build_spec = constants.RUN_DEPLOY_IN_SPOKE_BUILDSPEC.format(install_command)
+        build_spec = generated_manifest.get("build_spec")
+        spoke_execution_mode_deploy_env = generated_manifest.get(
+            "spoke_execution_mode_deploy_env"
+        )
+        vars = generated_manifest.get("vars")
 
         with self.spoke_client("codebuild") as codebuild:
             response = codebuild.start_build(
                 projectName=constants.EXECUTION_SPOKE_CODEBUILD_PROJECT_NAME,
                 environmentVariablesOverride=vars,
-                computeTypeOverride=self.spoke_execution_mode_deploy_env,
+                computeTypeOverride=spoke_execution_mode_deploy_env,
                 buildspecOverride=build_spec,
             )
         self.write_output(dict(account_id=self.account_id, **response))
